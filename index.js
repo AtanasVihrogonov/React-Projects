ee2c7bda538e934591854c6c2de8ac7e4bc1adf9
AV <vihrogonov@protonmail.com>
2021-02-17 16:18:11 +0000

    Render Page & Projects

---
+++ index.js
@@ -0,0 +1,41 @@
+const data = [
+  {
+    id: "01",
+    projectName: "#",
+    url: "www.avihrogonov.co.uk"
+  },
+  {
+    id: "02",
+    projectName: "#",
+    url: "www.avihrogonov.co.uk"
+  },
+]
+
+const renderProject = item => {
+  return `
+    <div class="col-md-6">
+      <div class="alert alert-secondary" role="alert">
+        ${item.id}. ${item.projectName}
+        <button
+          type="button"
+          class="btn bg-light text-dark py-0 float-right"
+        >
+          <a href="${item.url}"> Live Demo</a>
+        </button>
+      </div>
+    </div>
+  `;
+}
+
+const renderPage = () => {
+  const mainContentEl = document.querySelector("[data-selector='main-content']")
+  const html = `
+    <h1 class="p-4 text-center">React-Projects</h1>
+    <div class="row justify-content-center mt-5">
+      ${data.map(renderProject).join("")}
+    </div>
+  `;
+  mainContentEl.innerHTML = html
+}
+
+window.addEventListener('DOMContentLoaded', renderPage);
