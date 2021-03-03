5d12c93e7ad75849eaadb9a8b51a0e0fa8fbdd45
AV <vihrogonov@protonmail.com>
2021-03-03 15:49:54 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import Ball from './Ball';
-import Lottery from './Lottery';
-
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <Lottery />
-        <Lottery title="Sport TOTO 6/49" maxNum={49} numBalls={6} />
+
       </div>
     );
   }
